12b292d95bb27172983cc570f997c43c98822a04
Philippe Monteil <phil.monteil@laposte.net>
2020-04-26 10:15:53 +0200

    NuGet Veldrid.Views.CommandListFactory

---
+++ NUGet/Veldrid.Views.CommandListFactory/generate.bat
@@ -0,0 +1,12 @@
+
+copy ..\..\Veldrid.Views.CommandListFactory\bin\Release\netstandard2.0\Veldrid.Views.CommandListFactory.dll .\lib\netstandard2.0
+copy ..\..\Veldrid.Views.CommandListFactory\bin\Release\netstandard2.0\Veldrid.Views.CommandListFactory.pdb .\lib\netstandard2.0
+pause
+
+del Veldrid.Views.CommandListFactory.1.0.*.nupkg
+
+nuget pack Veldrid.Views.CommandListFactory.nuspec
+
+copy Veldrid.Views.CommandListFactory.1.0.*.nupkg ..
+
+pause
