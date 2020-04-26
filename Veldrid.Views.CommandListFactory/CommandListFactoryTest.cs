d2375faf8f7e1a337ae1b581700c6173df7e4550
Philippe Monteil <phil.monteil@laposte.net>
2020-04-26 09:05:29 +0200

    Veldrid.Views.Contracts + CommandListFactory

---
+++ Veldrid.Views.CommandListFactory/CommandListFactoryTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Veldrid;
+
+namespace Veldrid.Views.CommandListFactory
+{
+
+    public class CommandListFactoryTest : ICommandListFactory
+    {
+        public CommandListFactoryTest()
+        {
+        }
+
+        public CommandList BuildCommandList(GraphicsDevice graphicsDevice, Framebuffer framebuffer)
+        {
+            Veldrid.CommandList _commandList = null;
+            try
+            {
+                _commandList = graphicsDevice.ResourceFactory.CreateCommandList();
+                if (_commandList == null) return null;
+
+                _commandList.Begin();
+
+                _commandList.SetFramebuffer(framebuffer);
+                _commandList.SetFullViewports();
+
+                _commandList.ClearColorTarget(0, RgbaFloat.DarkRed);
+
+                _commandList.End();
+
+                Veldrid.CommandList vret = _commandList;
+                _commandList = null;
+
+                return vret;
+            }
+            finally
+            {
+                _commandList?.Dispose();
+            }
+        }
+    }
+
+}
---
+++ Veldrid.Views.Contracts/ICommandListFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Veldrid;
+
+namespace Veldrid.Views.Contracts
+{
+
+    public interface ICommandListFactory
+    {
+        Veldrid.CommandList BuildCommandList(Veldrid.GraphicsDevice graphicsDevice, Veldrid.Framebuffer framebuffer);
+    }
+
+}
