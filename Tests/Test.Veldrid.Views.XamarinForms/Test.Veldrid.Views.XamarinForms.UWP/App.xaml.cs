41fea263441be3c9849664d0e9916c42c742fb9f
Philippe Monteil <phil.monteil@laposte.net>
2020-04-26 14:31:04 +0200

    Test.Veldrid.Views.XamarinForms update

--- Tests/Test.Veldrid.Views.XamarinForms/Test.Veldrid.Views.XamarinForms.UWP/App.xaml.cs
+++ Tests/Test.Veldrid.Views.XamarinForms/Test.Veldrid.Views.XamarinForms.UWP/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
@@ -52,7 +53,14 @@ namespace Test.Veldrid.Views.XamarinForms.UWP
 
                 rootFrame.NavigationFailed += OnNavigationFailed;
 
-                Xamarin.Forms.Forms.Init(e);
+                // IMPORTANT
+                {
+                    var rendererAssemblies = new[]
+                    {
+                        typeof(global::Veldrid.Views.XamarinForms.UWP.VeldridViewRenderer).GetTypeInfo().Assembly,
+                    };
+                    Xamarin.Forms.Forms.Init(e, rendererAssemblies);
+                }
 
                 if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
                 {
